26aff348f46ccbe803a5f5b167ea5e88c156a75d
MargaritaEstevez <mmestevez@miuandes.cl>
2019-03-25 14:05:30 -0300

    views implementations

---
+++ app/src/main/java/com/example/laboratorio1/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.laboratorio1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View;
+import android.widget.EditText;
+import android.R
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun login(view: View){
+
+    }
+}
